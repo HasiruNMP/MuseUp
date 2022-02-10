99c1b8c35edaa676b2393a7dea2f3fb364e1b822
HasiruN <hasirunmp@gmail.com>
2022-02-10 17:13:46 +0530

    file

---
+++ musicianapp/lib/view/connections.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ConnectionsView extends StatefulWidget {
+  const ConnectionsView({Key? key}) : super(key: key);
+
+  @override
+  _ConnectionsViewState createState() => _ConnectionsViewState();
+}
+
+class _ConnectionsViewState extends State<ConnectionsView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
