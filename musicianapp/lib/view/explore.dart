24183d18728df35738b2f01f67d680b7ebf4081a
HasiruN <hasirunmp@gmail.com>
2022-02-11 00:11:01 +0530

    geo query basic

--- musicianapp/lib/view/explore.dart
+++ musicianapp/lib/view/explore.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:geoflutterfire/geoflutterfire.dart';
 import 'package:flutter/material.dart';
 
 class ExploreView extends StatefulWidget {
@@ -28,10 +29,17 @@ class UserInformation extends StatefulWidget {
 }
 
 class _UserInformationState extends State<UserInformation> {
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
   final Stream<QuerySnapshot> _usersStream = FirebaseFirestore.instance.collection('musicians').snapshots();
+
+  //Stream<List<DocumentSnapshot>> stream = geo.collection(collectionRef: collectionReference).within(center: center, radius: radius, field: field);
+  final geo = Geoflutterfire();
+  
 
   @override
   Widget build(BuildContext context) {
+    //setLocation();
+
     return StreamBuilder<QuerySnapshot>(
       stream: _usersStream,
       builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
@@ -46,13 +54,52 @@ class _UserInformationState extends State<UserInformation> {
         return ListView(
           children: snapshot.data!.docs.map((DocumentSnapshot document) {
             Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
-            return ListTile(
-              title: const Text('Name'),
-              subtitle: Text(data['name']),
+            return Column(
+              children: [
+            ListTile(
+            title: const Text('Name'),
+            subtitle: Text(data['name']),
+            ),
+                TextButton(
+                  style: ButtonStyle(
+                    foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+                  ),
+                  onPressed: getData,
+                  child: const Text('TextButton'),
+                )
+
+
+              ],
             );
           }).toList(),
         );
       },
     );
+  }
+//7.242016, 80.857134
+  void setLocation(){
+    GeoFirePoint myLocation = geo.point(latitude: 7.206750, longitude: 80.848153);
+    GeoFirePoint myLocation1 = geo.point(latitude: 7.195850, longitude: 80.677751);
+    GeoFirePoint myLocation2 = geo.point(latitude: 7.201300, longitude: 80.523840);
+    GeoFirePoint myLocation3 = geo.point(latitude: 6.955992, longitude: 80.276482);
+    GeoFirePoint myLocation4 = geo.point(latitude: 6.235698, longitude: 80.303966);
+    firestore.collection('locations').add({'name': 'random name', 'position': myLocation.data});
+    firestore.collection('locations').add({'name': 'random name', 'position': myLocation1.data});
+    firestore.collection('locations').add({'name': 'random name', 'position': myLocation2.data});
+    firestore.collection('locations').add({'name': 'random name', 'position': myLocation3.data});
+    firestore.collection('locations').add({'name': 'random name', 'position': myLocation4.data});
+  }
+
+  void getData(){
+    GeoFirePoint center = geo.point(latitude: 7.242016, longitude: 80.857134);
+    double radius = 2;
+    String field = 'position';
+    var collectionReference = firestore.collection('locations');
+    Stream<List<DocumentSnapshot>> stream = geo.collection(collectionRef: collectionReference).within(center: center, radius: radius, field: field);
+    stream.listen((List<DocumentSnapshot> documentList) {
+      for (var document in documentList) {
+        print(document.data());
+      }
+    });
   }
 }
